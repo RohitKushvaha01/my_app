787935f3d39d8f8f80037b23ca86574e62f6aebe
RohitKushvaha01 <oldisg131@gmail.com>
2023-10-03 11:49:49 +0530

    fist production build

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-//import EditBox from './editbox'
-
-
-/*function App() {const [inputValue, setInputValue] = useState('');
-
-  const handleInputChange = (event) => {
-    setInputValue(event.target.value);
-  };
- 
- 
-  return (
-    <>
-			<div className="logo" style={{top: `-${window.innerHeight/3}px`}}>
-        <h2 style={{width: '100%'}}>Insta-Save</h2>
-
-                              <input
-                    type="text"
-                    id="myInput"
-                    value={inputValue}
-                    onChange={handleInputChange}
-                    className="editbox"
-                    placeholder=" Paste Url"
-                />
-      <button onClick={cont}>Download</button>
-      </div>
-      
-
-     
-    </>
-  )
-}*/
+import {isMobile} from "./Utils"
 function App() {
   const [inputValue, setInputValue] = useState('');
-
+  const [loading,isLoading] = useState(false)
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
   };
 
-  const [data, setData] = useState(null);
+  
 
   function load(){
-  	if(!isURL()){
+  	if(!isURL(inputValue)){
   		//need to show an error
   		return
   	}
-    // Replace 'your-server-url' with the actual URL of your server.
-    fetch('https://rohit.cyclic.cloud/url?='+inputValue)
+    fetch('http://127.0.0.1:8080/url?='+inputValue)
       .then((response) => {
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
         }
         
         return response.text();
+        isLoading(false)
       })
       .then((result) => {
-        setData(result);
-        if(data!=null){
-        	download(data)
+        if(result!=null){
+        	download(result)
         }
+        isLoading(false)
       })
       .catch((error) => {
         console.error('Fetch error:', error);
+				isLoading(false)
       });
       function download(url) {
   if (isURL(url)) {
@@ -82,19 +51,18 @@ function App() {
     // Create an anchor element
     
 }
-			function isURL(str) {
-  const urlPattern = /^(http|https|ftp):\/\/([A-Z0-9_-]+\.)*[A-Z0-9_-]+\.[A-Z]{2,4}.*$/i;
-  return urlPattern.test(str);
-}
+		  function isURL(str){
+		  	const urlPattern = /^(http|https|ftp):\/\/([A-Z0-9_-]+\.)*[A-Z0-9_-]+\.[A-Z]{2,4}.*$/i;
+		  	
+  			return urlPattern.test(str);
+		  }
   }
-  
-  
 
   
 
   return (
     <>
-      <div className="logo" style={{ top: `-${window.innerHeight / 3}px` }}>
+      <div className="ctn" style={{ top: `-${window.innerHeight / 3}px` }}>
         <h2 style={{ width: '100%' }}>Insta-Save</h2>
         <input
           type="text"
@@ -104,10 +72,42 @@ function App() {
           className="editbox"
           placeholder="Paste Url"
         />
-        <button onClick={load}>Download</button>
+			
+			
+			<button className="btn"
+			
+			onClick={()=>{
+				isLoading(true)
+				load()
+			}}>
+
+			 {loading && (
+			 	    <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+			 	)}
+			 				<p className="dn"
+			 				style={{marginLeft: (()=>{
+			 					if(loading){
+			 						return (isMobile()) ? "26%" : "36%"
+			 					}
+			 					return (isMobile()) ? "36%" : "46%"
+			 				})()}}
+			 				>Download</p>
+			</button>
+        
       </div>
+      
+      
     </>
   );
 }
+
+/*
+{loading && (
+    <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+  )}
+  <p className="dn"
+    style={{marginLeft: (ismobile()) ? "30%" : "36%"}}
+*/
+
 
 export default App
